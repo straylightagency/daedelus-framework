b38151896a08f431e9d4ca6ea0c86af68ae8ad8b
Anthony Pauwels <anthony.pauwels.be@gmail.com>
2026-05-29 15:02:01 +0200

    fix: remove first / for path string on app_url and upload_url functions

--- helpers.php
+++ helpers.php
@@ -2,6 +2,7 @@
 
 use App\Models\User;
 use Illuminate\Support\Js;
+use Illuminate\Support\Str;
 use Daedelus\Framework\Mix;
 use Daedelus\Framework\Vite;
 use Illuminate\Http\Request;
@@ -17,16 +18,16 @@ use Daedelus\Framework\Cache\WordPressProxyCache;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 if ( ! function_exists( 'app_url' ) ) {
-	/**
-	 * The URL of the app.
-	 *
-	 * @param string $path
-	 * @return string
-	 */
-	function app_url(string $path = ''):string
-	{
-		return config( 'app.url' ) . ( $path != '' ? '/' . $path : '' );
-	}
+    /**
+     * The URL of the app.
+     *
+     * @param string $path
+     * @return string
+     */
+    function app_url(string $path = ''):string
+    {
+        return config( 'app.url' ) . ( $path != '' ? Str::start( $path, '/' ) : '' );
+    }
 }
 
 if ( ! function_exists('app_name') ) {
@@ -42,81 +43,81 @@ if ( ! function_exists('app_name') ) {
 }
 
 if ( ! function_exists( 'theme_path' ) ) {
-	/**
-	 * The path to the theme directory.
-	 *
-	 * @param string $path
-	 * @return string
-	 */
-	function theme_path(string $path = ''):string
-	{
-		return app()->themePath( $path );
-	}
+    /**
+     * The path to the theme directory.
+     *
+     * @param string $path
+     * @return string
+     */
+    function theme_path(string $path = ''):string
+    {
+        return app()->themePath( $path );
+    }
 }
 
 if ( ! function_exists( 'theme_url' ) ) {
-	/**
-	 * The URL to the theme directory.
-	 *
-	 * @param string $url
-	 * @return string
-	 */
-	function theme_url(string $url = ''):string
-	{
-		return app()->themeUrl( $url );
-	}
+    /**
+     * The URL to the theme directory.
+     *
+     * @param string $url
+     * @return string
+     */
+    function theme_url(string $url = ''):string
+    {
+        return app()->themeUrl( $url );
+    }
 }
 
 if ( ! function_exists( 'content_path' ) ) {
-	/**
-	 * The path to the content directory.
-	 *
-	 * @param string $path
-	 * @return string
-	 */
-	function content_path(string $path = ''):string
-	{
-		return app()->contentPath( $path );
-	}
+    /**
+     * The path to the content directory.
+     *
+     * @param string $path
+     * @return string
+     */
+    function content_path(string $path = ''):string
+    {
+        return app()->contentPath( $path );
+    }
 }
 
 if ( ! function_exists( 'public_content_url' ) ) {
-	/**
-	 * The URL to the content directory.
-	 *
-	 * @param string $path
-	 * @return string
-	 */
-	function public_content_url(string $path = ''):string
-	{
-		return app()->contentUrl( $path );
-	}
+    /**
+     * The URL to the content directory.
+     *
+     * @param string $path
+     * @return string
+     */
+    function public_content_url(string $path = ''):string
+    {
+        return app()->contentUrl( $path );
+    }
 }
 
 if ( ! function_exists( 'plugins_path' ) ) {
-	/**
-	 * The path to plugins.
-	 *
-	 * @param string $path
-	 * @return string
-	 */
-	function plugins_path(string $path = ''):string
-	{
-		return app()->pluginsPath( $path );
-	}
+    /**
+     * The path to plugins.
+     *
+     * @param string $path
+     * @return string
+     */
+    function plugins_path(string $path = ''):string
+    {
+        return app()->pluginsPath( $path );
+    }
 }
 
 if ( ! function_exists( 'mu_plugins_path' ) ) {
-	/**
-	 * The path to must-use plugins.
-	 *
-	 * @param string $path
-	 * @return string
-	 */
-	function mu_plugins_path(string $path = ''):string
-	{
-		return app()->muPluginsPath( $path );
-	}
+    /**
+     * The path to must-use plugins.
+     *
+     * @param string $path
+     * @return string
+     */
+    function mu_plugins_path(string $path = ''):string
+    {
+        return app()->muPluginsPath( $path );
+    }
 }
 
 if ( ! function_exists('upload_url') ) {
@@ -128,7 +129,7 @@ if ( ! function_exists('upload_url') ) {
      */
     function upload_url(string $path = ''):string
     {
-        return app_url( UPLOADS ) . ( $path != '' ? '/' . $path : '' );
+        return app_url( UPLOADS ) . ( $path != '' ? Str::start( $path, '/' ) : '' );
     }
 }
 
@@ -146,33 +147,33 @@ if ( ! function_exists('upload_path') ) {
 }
 
 if ( ! function_exists( 'is_debug' ) ) {
-	/**
-	 * If debug mode is enabled
-	 *
-	 * @return bool
-	 */
-	function is_debug():bool
-	{
-		return config( 'app.debug', false );
-	}
+    /**
+     * If debug mode is enabled
+     *
+     * @return bool
+     */
+    function is_debug():bool
+    {
+        return config( 'app.debug', false );
+    }
 }
 
 if ( ! function_exists( 'render' ) ) {
-	/**
-	 * Define the render callback for the template.
-	 *
-	 * @param Closure $callback
-	 *
-	 * @return ViewOptions
-	 */
-	function render(Closure $callback): ViewOptions
-	{
-		app( ViewScanner::class )->whenListening(
-			fn () => ViewMetadata::instance()->renders[] = $callback
-		);
+    /**
+     * Define the render callback for the template.
+     *
+     * @param Closure $callback
+     *
+     * @return ViewOptions
+     */
+    function render(Closure $callback): ViewOptions
+    {
+        app( ViewScanner::class )->whenListening(
+            fn () => ViewMetadata::instance()->renders[] = $callback
+        );
 
-		return new ViewOptions;
-	}
+        return new ViewOptions;
+    }
 }
 
 if ( ! function_exists('withFields') ) {
@@ -184,14 +185,14 @@ if ( ! function_exists('withFields') ) {
      *
      * @return ViewOptions
      */
-	function withFields(Closure|null $callback = null, string $key = 'fields'): ViewOptions
-	{
+    function withFields(Closure|null $callback = null, string $key = 'fields'): ViewOptions
+    {
         if ( $callback ) {
             fields( $callback );
         }
 
-		return render( fn ( WP_post $post ) => [ $key => get_fields( $post->ID ) ] );
-	}
+        return render( fn ( WP_post $post ) => [ $key => get_fields( $post->ID ) ] );
+    }
 }
 
 if ( ! function_exists( 'fields' ) ) {
@@ -213,151 +214,151 @@ if ( ! function_exists( 'fields' ) ) {
 }
 
 if ( ! function_exists('withPost') ) {
-	/**
-	 * Define a default render callback for the template that pass the WP_Post to the view.
-	 *
-	 * @param string $key
-	 *
-	 * @return ViewOptions
-	 */
-	function withPost(string $key = 'post'): ViewOptions
-	{
-		return render( fn ( WP_post $post ) => [ $key => $post ] );
-	}
+    /**
+     * Define a default render callback for the template that pass the WP_Post to the view.
+     *
+     * @param string $key
+     *
+     * @return ViewOptions
+     */
+    function withPost(string $key = 'post'): ViewOptions
+    {
+        return render( fn ( WP_post $post ) => [ $key => $post ] );
+    }
 }
 
 if ( ! function_exists( 'middleware' ) ) {
-	/**
-	 * Define the middleware of the template.
-	 *
-	 * @param array $middleware
-	 *
-	 * @return ViewOptions
-	 */
-	function middleware(array $middleware): ViewOptions
-	{
-		app( ViewScanner::class )->whenListening(
-			fn () => ViewMetadata::instance()->middleware = $middleware
-		);
+    /**
+     * Define the middleware of the template.
+     *
+     * @param array $middleware
+     *
+     * @return ViewOptions
+     */
+    function middleware(array $middleware): ViewOptions
+    {
+        app( ViewScanner::class )->whenListening(
+            fn () => ViewMetadata::instance()->middleware = $middleware
+        );
 
-		return new ViewOptions;
-	}
+        return new ViewOptions;
+    }
 }
 
 if ( ! function_exists( 'name' ) ) {
-	/**
-	 * Define the name of the template.
-	 *
-	 * @param string $name
-	 *
-	 * @return ViewOptions
-	 */
-	function name(string $name): ViewOptions
-	{
-		app( ViewScanner::class )->whenListening(
-			fn () => ViewMetadata::instance()->name = $name
-		);
+    /**
+     * Define the name of the template.
+     *
+     * @param string $name
+     *
+     * @return ViewOptions
+     */
+    function name(string $name): ViewOptions
+    {
+        app( ViewScanner::class )->whenListening(
+            fn () => ViewMetadata::instance()->name = $name
+        );
 
-		return new ViewOptions;
-	}
+        return new ViewOptions;
+    }
 }
 
 if ( ! function_exists( 'type' ) ) {
-	/**
-	 * Define the post type for the template.
-	 *
-	 * @param string $type
-	 *
-	 * @return ViewOptions
-	 */
-	function type(string $type): ViewOptions
-	{
-		app( ViewScanner::class )->whenListening(
-			fn () => ViewMetadata::instance()->type = $type
-		);
+    /**
+     * Define the post type for the template.
+     *
+     * @param string $type
+     *
+     * @return ViewOptions
+     */
+    function type(string $type): ViewOptions
+    {
+        app( ViewScanner::class )->whenListening(
+            fn () => ViewMetadata::instance()->type = $type
+        );
 
-		return new ViewOptions;
-	}
+        return new ViewOptions;
+    }
 }
 
 if ( ! function_exists('page_title') ) {
-	/**
-	 * Define the page title during rendering.
-	 *
-	 * @param string $page_title
-	 *
-	 * @return void
-	 */
-	function page_title(string $page_title): void
-	{
-		app( ViewScanner::class )->whenRendering(
-			function () use ( $page_title ) {
-				Filters::add('document_title_parts', function ($title) use ($page_title) {
-					$title['title'] = $page_title;
+    /**
+     * Define the page title during rendering.
+     *
+     * @param string $page_title
+     *
+     * @return void
+     */
+    function page_title(string $page_title): void
+    {
+        app( ViewScanner::class )->whenRendering(
+            function () use ( $page_title ) {
+                Filters::add('document_title_parts', function ($title) use ($page_title) {
+                    $title['title'] = $page_title;
 
-					return $title;
-				} );
-			}
-		);
-	}
+                    return $title;
+                } );
+            }
+        );
+    }
 }
 
 if ( ! function_exists( 'abort_404' ) ) {
-	/**
-	 * Tell both WordPress and Laravel to handle a 404.
-	 *
-	 * @return void
-	 */
-	function abort_404():void
-	{
-		global $wp_query;
-		$wp_query->set_404();
+    /**
+     * Tell both WordPress and Laravel to handle a 404.
+     *
+     * @return void
+     */
+    function abort_404():void
+    {
+        global $wp_query;
+        $wp_query->set_404();
 
-		throw new NotFoundHttpException();
-	}
+        throw new NotFoundHttpException();
+    }
 }
 
 if ( ! function_exists( 'vite' ) ) {
-	/**
-	 * Process Vite on assets entries.
-	 *
-	 * @param array|string $entries
-	 *
-	 * @return string
-	 * @throws Exception
-	 */
-	function vite(array|string $entries): string
-	{
-		return app( Vite::class )->asset( $entries );
-	}
+    /**
+     * Process Vite on assets entries.
+     *
+     * @param array|string $entries
+     *
+     * @return string
+     * @throws Exception
+     */
+    function vite(array|string $entries): string
+    {
+        return app( Vite::class )->asset( $entries );
+    }
 }
 
 if ( ! function_exists( 'mix' ) ) {
-	/**
-	 * Process Mix on assets entries.
-	 *
-	 * @param array|string $entries
-	 *
-	 * @return string
-	 * @throws Exception
-	 */
-	function mix(array|string $entries): string
-	{
-		return app( Mix::class )->asset( $entries );
-	}
+    /**
+     * Process Mix on assets entries.
+     *
+     * @param array|string $entries
+     *
+     * @return string
+     * @throws Exception
+     */
+    function mix(array|string $entries): string
+    {
+        return app( Mix::class )->asset( $entries );
+    }
 }
 
 if ( ! function_exists( 'option' ) ) {
-	/**
+    /**
      * Get an option field.
      *
-	 * @param string $field
-	 * @param mixed|null $default
-	 *
-	 * @return mixed
-	 */
-	function option(string $field, mixed $default = null):mixed
-	{
+     * @param string $field
+     * @param mixed|null $default
+     *
+     * @return mixed
+     */
+    function option(string $field, mixed $default = null):mixed
+    {
         $keys = [];
 
         if ( str_contains( $field, '.' ) ) {
@@ -365,69 +366,69 @@ if ( ! function_exists( 'option' ) ) {
             $field = array_shift( $keys );
         }
 
-		if ( $value = get_field( $field, 'option') ) {
+        if ( $value = get_field( $field, 'option') ) {
             if ( !empty( $keys ) ) {
                 foreach ( $keys as $key ) {
                     $value = $value[ $key ] ?? null;
                 }
             }
 
-			return $value;
-		}
+            return $value;
+        }
 
-		return $default;
-	}
+        return $default;
+    }
 }
 
 if ( ! function_exists( 'wp_cache' ) ) {
-	/**
+    /**
      * Return the Proxy Cache.
      *
-	 * @return WordPressProxyCache
-	 */
-	function wp_cache():WordPressProxyCache
-	{
-		return app( WordPressProxyCache::class );
-	}
+     * @return WordPressProxyCache
+     */
+    function wp_cache():WordPressProxyCache
+    {
+        return app( WordPressProxyCache::class );
+    }
 }
 
 if ( ! function_exists( 'menu' ) ) {
-	/**
-	 * @param string|null $menu_name
-	 *
-	 * @return MenuManager|Menu
-	 * @throws \Daedelus\Theme\Menus\Exceptions\MenuNotFoundException
-	 */
-	function menu(?string $menu_name):MenuManager|Menu
-	{
-		/** @var MenuManager $manager */
-		$manager = app( MenuManager::class );
+    /**
+     * @param string|null $menu_name
+     *
+     * @return MenuManager|Menu
+     * @throws \Daedelus\Theme\Menus\Exceptions\MenuNotFoundException
+     */
+    function menu(?string $menu_name):MenuManager|Menu
+    {
+        /** @var MenuManager $manager */
+        $manager = app( MenuManager::class );
 
-		if ( $menu_name ) {
-			return $manager->get( $menu_name );
-		}
+        if ( $menu_name ) {
+            return $manager->get( $menu_name );
+        }
 
-		return $manager;
-	}
+        return $manager;
+    }
 }
 
 if ( ! function_exists( 'get_home_page' ) ) {
-	/**
+    /**
      * Get the home page WP_Post.
      *
-	 * @param bool $with_fields
-	 * @return WP_Post
-	 */
-	function get_home_page(bool $with_fields = false): WP_Post
-	{
-		$page = get_post( get_option('page_on_front') );
+     * @param bool $with_fields
+     * @return WP_Post
+     */
+    function get_home_page(bool $with_fields = false): WP_Post
+    {
+        $page = get_post( get_option('page_on_front') );
 
-		if ( $with_fields ) {
-			$page->fields = get_fields( $page->ID );
-		}
+        if ( $with_fields ) {
+            $page->fields = get_fields( $page->ID );
+        }
 
-		return $page;
-	}
+        return $page;
+    }
 }
 
 if ( ! function_exists('get_admin_post_url') ) {
@@ -443,62 +444,62 @@ if ( ! function_exists('get_admin_post_url') ) {
 }
 
 if ( ! function_exists( 'is_local' ) ) {
-	/**
-	 * Return if the app is in local mode or not.
-	 *
-	 * @return bool
-	 */
-	function is_local(): bool
-	{
-		return app()->environment('local');
-	}
+    /**
+     * Return if the app is in local mode or not.
+     *
+     * @return bool
+     */
+    function is_local(): bool
+    {
+        return app()->environment('local');
+    }
 }
 
 if ( ! function_exists( 'is_staging' ) ) {
-	/**
-	 * Return if the app is in staging mode or not.
-	 *
-	 * @return bool
-	 */
-	function is_staging(): bool
-	{
-		return app()->environment('staging');
-	}
+    /**
+     * Return if the app is in staging mode or not.
+     *
+     * @return bool
+     */
+    function is_staging(): bool
+    {
+        return app()->environment('staging');
+    }
 }
 
 if ( ! function_exists( 'is_production' ) ) {
-	/**
-	 * Return if the app is in production mode or not.
-	 *
-	 * @return bool
-	 */
-	function is_production(): bool
-	{
-		return app()->environment('production');
-	}
+    /**
+     * Return if the app is in production mode or not.
+     *
+     * @return bool
+     */
+    function is_production(): bool
+    {
+        return app()->environment('production');
+    }
 }
 
 if ( ! function_exists( 'export_routes' ) ) {
-	/**
-	 * Export application routes url by name.
-	 *
-	 * @param array $which
-	 * @return array
-	 */
-	function export_routes(array $which = []): array
-	{
-		$routes = collect( app('router')->getRoutes()->getRoutesByName() );
+    /**
+     * Export application routes url by name.
+     *
+     * @param array $which
+     * @return array
+     */
+    function export_routes(array $which = []): array
+    {
+        $routes = collect( app('router')->getRoutes()->getRoutesByName() );
 
-		if ( !empty( $which ) ) {
-			$routes = $routes->only( $which );
-		}
+        if ( !empty( $which ) ) {
+            $routes = $routes->only( $which );
+        }
 
-		$routes = $routes->mapWithKeys( function ( Route $value, $key ) {
-			return [ $key => $value->uri() ];
-		} );
+        $routes = $routes->mapWithKeys( function ( Route $value, $key ) {
+            return [ $key => $value->uri() ];
+        } );
 
-		return $routes->toArray();
-	}
+        return $routes->toArray();
+    }
 }
 
 if ( ! function_exists('carbon') ) {
@@ -569,17 +570,17 @@ if ( ! function_exists('use_locale') ) {
 }
 
 if ( ! function_exists('json_from') ) {
-	/**
-	 * Transform an array or an object into JSON.
-	 *
-	 * @param $data
-	 * @return Js
-	 * @throws JsonException
-	 */
-	function json_from($data): Js
-	{
-		return Js::from( $data );
-	}
+    /**
+     * Transform an array or an object into JSON.
+     *
+     * @param $data
+     * @return Js
+     * @throws JsonException
+     */
+    function json_from($data): Js
+    {
+        return Js::from( $data );
+    }
 }
 
 if ( ! function_exists('force_logout') ) {
@@ -657,7 +658,9 @@ if ( ! function_exists('image_srcset') ) {
             return (int) $aMatch[1] <=> (int) $bMatch[1];
         } );
 
-        return implode(', ', array_unique( $srcset ) );
+        $srcset = array_unique( $srcset );
+
+        return implode(', ', $srcset );
     }
 }
 
